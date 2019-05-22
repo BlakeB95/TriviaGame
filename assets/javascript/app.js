06a5213b2259337b902a55c40e74bca6ecb078d7
Blake Bennett <blakebennettc@gmail.com>
2019-05-22 16:29:35 -0500

    finished

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     $("#start").click(function(){
 
         //start timer
-        var timeleft = 10;
+        var timeleft = 60;
         var downloadTimer = setInterval(function(){
             $("#countdown").html(timeleft + " seconds remaining");
             timeleft -= 1;
@@ -31,15 +31,45 @@ $(document).ready(function() {
 
         $(".questions").append("<div id='secondQuestion'>");
         $("#secondQuestion").append("<form>");
-            $("#secondQuestion").append("<h6>Who was the lead singer of Alice in Chains?</h6>");
-            $("#secondQuestion").append("<input type='radio' name='secAns' value='y'> Chris Cornell ");
-            $("#secondQuestion").append("<input type='radio' name='secAns' value='n'> Scott Weiland ");
-            $("#secondQuestion").append("<input type='radio' name='secAns' value='n'> Layne Stanley ");
+            $("#secondQuestion").append("<h6>Which album did the band Nirvana release first?</h6>");
+            $("#secondQuestion").append("<input type='radio' name='secAns' value='n'> Nevermind ");
+            $("#secondQuestion").append("<input type='radio' name='secAns' value='y'> Bleach ");
+            $("#secondQuestion").append("<input type='radio' name='secAns' value='n'> In Utero ");
             $("#secondQuestion").append("</form>");
         $("#secondQuestion").append("</div");
         $(".questions").append("</div>");
 
-        $(".questions").append("<div><br><p><input id='getAns' type='button' value='Get Answers'></p></div>")
+        $(".questions").append("<div id='thirdQuestion'><br>");
+        $("#thirdQuestion").append("<form>");
+        $("#thirdQuestion").append("<h6>In Stone Temple Pilots' <i>Creep</i>, what did they take time with?</h6>");
+            $("#thirdQuestion").append("<input type='radio' name='thirdAns' value='n'> A grounded man ");
+            $("#thirdQuestion").append("<input type='radio' name='thirdAns' value='y'> A wounded hand ");
+            $("#thirdQuestion").append("<input type='radio' name='thirdAns' value='n'> A larger land ");
+            $("#thirdQuestion").append("<form>");
+        $("#thirdQuestion").append("<div>");
+        $(".questions").append("</div>");
+
+        $(".questions").append("<div id='fourthQuestion'><br>");
+        $("#fourthQuestion").append("<form>");
+        $("#fourthQuestion").append("<h6>During Nirvana's MTV Unplugged performance, what band did they repeatedly cover?</h6>");
+            $("#fourthQuestion").append("<input type='radio' name='fourthAns' value='y'> Meat Puppets ");
+            $("#fourthQuestion").append("<input type='radio' name='fourthAns' value='n'> Led Zeppelin ");
+            $("#fourthQuestion").append("<input type='radio' name='fourthAns' value='n'> Fugazi ");
+            $("#fourthQuestion").append("<form>");
+        $("#fourthQuestion").append("<div>");
+        $(".questions").append("</div>");
+
+        $(".questions").append("<div id='fifthQuestion'><br>");
+        $("#fifthQuestion").append("<form>");
+        $("#fifthQuestion").append("<h6>The supergroup Temple of the Dog was created to honor what late musician?</h6>");
+            $("#fifthQuestion").append("<input type='radio' name='fifthAns' value='y'> Andrew Wood ");
+            $("#fifthQuestion").append("<input type='radio' name='fifthAns' value='n'> Layne Stanley ");
+            $("#fifthQuestion").append("<input type='radio' name='fifthAns' value='n'> Kurt Cobain ");
+            $("#fifthQuestion").append("<form>");
+        $("#fifthQuestion").append("<div>");
+        $(".questions").append("</div>");
+
+        $(".questions").append("<div><br><p><input id='getAns' type='button' value='Finish'></p></div>")
         
         //calc score and display to user
         var vals = [];
@@ -53,7 +83,10 @@ $(document).ready(function() {
         $("input[id='getAns']").click(function() {
             firstVal = $('input:radio[name=firstAns]:checked').val();
             secVal = $('input:radio[name=secAns]:checked').val();
-            vals = [firstVal, secVal];
+            thirdVal = $('input:radio[name=thirdAns]:checked').val();
+            fourthVal = $('input:radio[name=fourthAns]:checked').val();
+            fifthVal = $('input:radio[name=fifthAns]:checked').val();
+            vals = [firstVal, secVal, thirdVal, fourthVal, fifthVal];
 
             for(i=0;i<vals.length;i++){
                 if(vals[i] == "y"){
@@ -74,7 +107,10 @@ $(document).ready(function() {
         function finish(){
             firstVal = $('input:radio[name=firstAns]:checked').val();
             secVal = $('input:radio[name=secAns]:checked').val();
-            vals = [firstVal, secVal];
+            thirdVal = $('input:radio[name=thirdAns]:checked').val();
+            fourthVal = $('input:radio[name=fourthAns]:checked').val();
+            fifthVal = $('input:radio[name=fifthAns]:checked').val();
+            vals = [firstVal, secVal, thirdVal, fourthVal, fifthVal];
 
             for(i=0;i<vals.length;i++){
                 if(vals[i] == "y"){
